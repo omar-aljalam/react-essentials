01a5c17c67f34a855be0215fa46b4324307dfb17
Omar <omar-aljalam@outlook.com>
2026-01-09 16:04:37 +0200

    Working with State to display data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import { CORE_CONCEPTS } from "./data";
+import { useState } from "react";
+
+import { CORE_CONCEPTS, EXAMPLES } from "./data";
 import Header from "./components/Header/Header.jsx";
 import CoreConceptList from "./components/CoreConcept/CoreConceptList.jsx";
 import TabButton from "./components/TabButton.jsx";
 
 function App() {
+  const [ selectedTopic, setSelectedTopic ] = useState("components");
+
+  function handleClick(selectedButton) {
+    setSelectedTopic(selectedButton);
+  }
+
   return (
     <div>
       <Header />
@@ -17,11 +25,20 @@ function App() {
           <section id="examples">
             <h2>Examples</h2>
             <menu>
-              <TabButton>Components</TabButton>
-              <TabButton>JSX</TabButton>
-              <TabButton>Props</TabButton>
-              <TabButton>State</TabButton>
+              <TabButton onSelect={() => handleClick("components")}>Components</TabButton>
+              <TabButton onSelect={() => handleClick("jsx")}>JSX</TabButton>
+              <TabButton onSelect={() => handleClick("props")}>Props</TabButton>
+              <TabButton onSelect={() => handleClick("state")}>State</TabButton>
             </menu>
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+                {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+          </div>
           </section>
       </main>
     </div>
--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,12 +1,7 @@
-export default function TabButton(props) {
-    function handleClick() {
-        console.log("HI");
-        
-    }
-
+export default function TabButton({children, onSelect}) {
     return (
         <li>
-            <button onClick={handleClick} >{props.children}</button>
+            <button onClick={onSelect} >{children}</button>
         </li>
     );
 }
